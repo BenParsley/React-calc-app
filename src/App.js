12583b0f64e2c61b41a1a5b4b4f3aef521bb4098
BenParsley <32735272+BenParsley@users.noreply.github.com>
2026-06-30 16:25:46 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { useState, useRef } from "react";
+import "./App.css";
+
+function App() {
+    const inputRef = useRef(null);
+    const resultRef = useRef(null);
+    const [result, setResult] = useState(0);
+
+    function plus(e) {
+      e.preventDefault();
+      const inputVal = inputRef.current.value;
+      const newResult = result + Number(inputVal);
+      setResult(newResult);
+    }
+
+    function minus(e) { 
+      e.preventDefault();
+      const inputVal = inputRef.current.value;
+      const newResult = result - Number(inputVal);
+      setResult(newResult); 
+    }
+ 
+    function times(e) { 
+      e.preventDefault();
+      const inputVal = inputRef.current.value;
+      const newResult = result * Number(inputVal);
+      setResult(newResult); 
+    }
+  
+    function divide(e) { 
+      e.preventDefault();
+      const inputVal = Number(inputRef.current.value);
+
+      if (inputVal === 0) {
+        alert("Cannot divide by zero");
+        return;
+      }
+
+      setResult(result / inputVal);
+    }
+ 
+    function resetInput(e) { 
+      e.preventDefault()
+      inputRef.current.value = ""
+    }; 
+  
+    function resetResult(e) { 
+      e.preventDefault();
+      setResult((0));
+    }
+ 
+  return ( 
+    <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+
+      <form> 
+        
+        <p ref={resultRef} className="result">{result}</p>
+        
+        <input
+          pattern="[0-9]*" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        />
+        <div className="buttons">
+          <button onClick={plus}>add</button>
+          <button onClick={minus}>minus</button>
+          <button onClick={times}>times</button>
+          <button onClick={divide}>divide</button>
+          <button onClick={resetInput} className="reset">Reset Input</button>
+          <button onClick={resetResult} className="reset">Reset Result</button>
+        </div>
+ 
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
